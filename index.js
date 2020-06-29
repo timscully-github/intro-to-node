de92f0799457b305c5e8a6ae884514dea617c9ae
Tim Scully <tim.scully@wundermanthompson.com>
2020-06-29 15:37:18 +0100

    created simple node project with NPM to show how packages work

---
+++ index.js
@@ -0,0 +1,12 @@
+// jshint esversion:6
+
+// require the packages after using NPM to install
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+// find usable functions from the package documentation
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+// test the package
+console.log("My Superhero name is " + mySuperheroName + ", but my Supervillain name is " + mySupervillainName);
